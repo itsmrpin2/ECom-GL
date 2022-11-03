59859e53f6d76759a78d1bf492647606307e8906
Mr. Pintu <itsmrpin2@gmail.com>
2022-11-03 10:45:51 +0530

    ServiceImpl Created

---
+++ Product/src/main/java/com/rk/product/service/ProductServiceImpl.java
@@ -0,0 +1,13 @@
+package com.rk.product.service;
+
+import com.rk.product.dto.Product;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public String addProduct(Product product) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
